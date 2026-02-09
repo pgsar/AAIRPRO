073c6b1598469fbfa01c2ab647a588b971f6fa5c
pgsar <pierluigi@psartori.it>
2026-02-09 21:42:16 +0100

    Delete css directory

--- css/js/block.js
+++
@@ -1,6 +0,0 @@
-    // Blocca il tasto destro e debug
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    document.onkeydown = function(e) {
-        if(e.keyCode == 123) return false; 
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
-    };
